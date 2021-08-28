93c5994b5ad97d9bb488a511f63f4b1509eddaca
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-08-28 06:54:11 -0700

    feature: added rating stars

--- reviews-app/screens/ReviewDetails.js
+++ reviews-app/screens/ReviewDetails.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, Image, FlatList } from 'react-native'
 import globalStyles from '../styles/globalStyles'
 import Card from '../shared/card'
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function ReviewDetails({navigation}){
-  const pressHandler = ()=>{
-    navigation.goBack()
-  }
+  const [rating, setRating] = useState(navigation.getParam('rating'))
+ 
   return(
     <View style={globalStyles.bodyContainer}>
       <Card>
         <Text>Title: {navigation.getParam('title')}</Text>
         <Text>Body: {navigation.getParam('body')}</Text>
-        <Text>Rating: {navigation.getParam('rating')}</Text>
+        <Text>{rating}</Text>
+        <View style={styles.rating}>
+          <Text>Movie Rating: </Text>
+          <View style={styles.stars}>
+          {(()=>{
+            let star = []
+            for(let i=1; i<=rating; i++){
+              star.push(<FontAwesome name="star" size={24} color="gold" />)
+            }
+            return star
+          })()}
+          </View>
+        </View>
       </Card>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  
+  stars:{
+    flexDirection: 'row'
+  }
 })
